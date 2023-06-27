85ebcfbb43c143d12f48eb10be4078d5aaf42e5e
Niels <niels.vder.vlist@gmail.com>
2023-06-27 16:26:50 +0200

    Keine anhung

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,10 @@ import Hero from '@components/layout/Hero'
 import Intro from '@components/layout/Intro'
 import Projects from '@components/layout/Projects'
 import Solutions from '@components/about/Solutions'
-import {ParallaxProvider} from 'react-scroll-parallax'
-import PatternLayout from '@components/containers/PatternLayout'
-import MotionTracker from '@components/containers/MotionTracker'
 import Contact from '@components/contact/Contact'
 
 export default function Home() {
-    return (
+    return <>
         <main className="flex min-h-screen flex-col items-center justify-between">
             <Hero/>
             <Intro/>
@@ -17,5 +14,5 @@ export default function Home() {
             <Projects/>
             <Contact/>
         </main>
-    )
+    </>
 }
